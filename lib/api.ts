4f6df026b1fad1d517c70c9f5138b50cd489f7e1
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2025-08-04 21:23:01 +0300

    +

--- lib/api.ts
+++ lib/api.ts
@@ -18,12 +18,30 @@ export type NoteListType = {
   total: number
 }
 
-export const getNotes = async () => {
-  const { data } = await axios.get<NoteListType>('/notes')
+export type CategoryType = {
+  id: string
+  name: string
+  description: string
+  createdAt: string
+  updatedAt: string
+}
+
+export const getNotes = async (categoryId?: string, title?: string) => {
+  const { data } = await axios.get<NoteListType>('/notes', {
+    params: {
+      categoryId,
+      title: title,
+    },
+  })
   return data
 }
 
 export const getSingleNote = async (id: string) => {
   const { data } = await axios.get<NoteType>(`/notes/${id}`)
+  return data
+}
+
+export const getCategories = async () => {
+  const { data } = await axios.get<CategoryType[]>(`/categories`)
   return data
 }
